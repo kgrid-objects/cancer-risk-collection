b761f5e52f4397986008002001f85eb9be8eed8a
Greg Farris <greg.t.farris@gmail.com>
2018-08-08 14:57:11 -0400

    Added Package feature that will create all zip and each ko zip

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env sh
 
-zip -r $1.zip $1
+# Setup
+rm -rf dist
+mkdir -p dist
+cp -r 99999-* dist
+
+# create zip for each ko
+cd dist
+for d in * ; do
+    echo "$d.zip"
+    zip -r -X "$d.zip" $d -x \"*.DS_Store\"
+done
+
+# create one big zip of all kos
+zip -r -X cancer-risk.zip * -x \"*.DS_Store\" "*.zip*"
+
+# clean up only leaving zip files
+rm -r */
