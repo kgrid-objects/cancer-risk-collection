79666cb8e840dd93fd6de2a71f348e4c363907f5
Greg Farris <greg.t.farris@gmail.com>
2018-08-10 14:50:05 -0400

    Added beginning tests

---
+++ tests/sixyearprobability.test.js
@@ -0,0 +1,43 @@
+var rewire = require('rewire');
+
+//Get load in the js
+var javascript = rewire('../99999-fk4jh3tk9s/v0.0.2/model/resource/getSixyearprobability');
+
+//Load in the function
+var sixyearprobability = javascript.__get__("getSixyearprobability");
+
+
+test('Test the Testing', () => {
+  expect(1).toBe(1);
+});
+
+test('happy day', () => {
+
+  var payload = sixyearprobability({
+    "age": 65,
+    "ethnicity": 1,
+    "bmi": 27,
+    "cigsPerDay": 10,
+    "edLevel": 1,
+    "hxLungCancer": 1,
+    "hxLungCancerFam": 1,
+    "hxNonLungCancerDz": 1,
+    "yrsQuit": 1,
+    "yrsSmoker": 20
+  });
+
+  expect(payload.success).toBe(1);
+  expect(payload.result).toBe(2.442115272387271);
+
+});
+
+
+test('no data throws exception', () => {
+
+  expect(() => {
+    sixyearprobability()
+  }).toThrow();
+
+});
+
+
