6d6ca66588bc6087ea5da6a7bda4f5c3e4860e4a
circleci <kgrid-developers@umich.edu>
2019-01-09 21:56:00 -0500

    [#162812221] Changed KOIO Implementation metadata to use Implementation context not ko context and moved KOs to collection folder

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env sh
 
-# Setup
-rm -rf dist
-mkdir -p dist
-cp -r 99999-* dist
-
-# create zip for each ko
-cd dist
+cd collection
 for d in * ; do
     echo "$d.zip"
-    zip -r -X "$d.zip" $d -x \"*.DS_Store\"
+    zip -r -X "../$d.zip" $d -x \"*.DS_Store\"
 done
-
-# create one big zip of all kos
-zip -r -X cancer-risk.zip * -x \"*.DS_Store\" "*.zip*"
-
-# clean up only leaving zip files
-rm -r */
